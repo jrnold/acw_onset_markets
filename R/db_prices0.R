b55dbc99bc4084385bfb4ef2fd7bc818283a989c
jrnold <jeffrey.arnold@gmail.com>
2014-06-09 13:12:26 -0400

    misc

--- R/db_prices0.R
+++ R/db_prices0.R
@@ -18,51 +18,75 @@ BANKERS_FILE <- PROJ$path("submodules/civil_war_era_findata/data/bankers_magazin
 .DEPENDENCIES <- c(BANKERS_FILE,
                    MERCHANTS_FILE)
 
+RENAME_BANKERS <-
+    c("California 7 per cents, 1870" = "California 7s, 1870",
+      "California 7 per cents, 1877" = "California 7s, 1877",
+      "Georgia 6 per cents" = "Georgia 6s",
+      "Indiana 5 per cents" = "Indiana 5s",
+      "Indiana 6 per cents" = "Indiana 6s",
+      "Kentucky 6 per cents" = "Kentucky 6s",
+      "Louisiana 6 per cents" = "Louisiana 6s",
+      "Missouri 6 per cents" = "Missouri 6s",
+      "North Carolina 6 per cents" = "North Carolina 6s",
+      "Ohio 6 per cents, 1875" = "Ohio 6s, 1875",
+      "Ohio 6 per cents, 1886" = "Ohio 6s, 1886",
+      "Pennsylvania 5 per cents" = "Pennsylvania 5s",
+      "Tennessee 6 per cents" = "Tennessee 6s",
+      "U.S. 5 per cents, 1874" = "U.S. 5s, 1874",
+      "U.S. 6 per cents, 1867-8" = "U.S. 6s, 1868",
+      "U.S. 6 per cents, 1881" = "U.S. 6s, 1881",
+      "Virginia 6 per cents" = "Virginia 6s"
+      )
+
+RENAME_MERCHANTS <-
+    c("10-40's" = "U.S. 10-40s",
+      "1 year certificate, New" = "U.S. 1-year, 1862",
+      "1 year certificate, Old" = "U.S. 1-year, 1863",
+      "5-20's, Coup." = "U.S. 5-20s",
+      "6's, 1881 Coup." = "U.S. 6s, 1881",
+      "7 3-10, 3 years" = "U.S. 7-30s")
+
 BOND_GROUPS <-
-    c("california_7pct_1870" = "North",
-      "california_7pct_1877" = "North",
-      "georgia_6pct" = "South",
-      "indiana_5pct" = "North",
-      "indiana_6pct" = "North",
-      "kentucky_6pct" = "Border",
-      "louisiana_6pct" = "South",
-      "missouri_6pct" = "Border",
-      "north_carolina_6pct" = "South",
-      "ohio_6pct_1875" = "North",
-      "ohio_6pct_1886" = "North",
-      "pennsylvania_5pct" = "North",
-      "tennessee_6pct" = "South",
-      "US_5pct_1874" = "Union",
-      "US_6pct_1868" = "Union",
-      "US_6pct_1881" = "Union",
-      "virginia_6pct" = "South",
-      "US_five_twenty" = "Union",
-      "US_oneyr_new" = "Union",
-      "US_oneyr_old" = "Union",
-      "US_seven_thirty" = "Union",
-      "US_ten_forty" = "Union")
+    c("California 7's 1870" = "North",
+      "California 7's 1877" = "North",
+      "Georgia 6's" = "South",
+      "Indiana 5's" = "North",
+      "Indiana 6's" = "North",
+      "Kentucky 6's" = "Border",
+      "Louisiana 6's" = "South",
+      "Missouri 6's" = "Border",
+      "North Carolina 6's" = "South",
+      "Ohio 6's, 1875" = "North",
+      "Ohio 6's, 1886" = "North",
+      "Pennsylvania 5's" = "North",
+      "Tennessee 6's" = "South",
+      "US 5's, 1874" = "Union",
+      "US 6's, 1868" = "Union",
+      "US 6's, 1881" = "Union",
+      "Virginia 6's" = "South",
+      "US 5-20's" = "Union",
+      "US 1-year (old)" = "Union",
+      "US 1-year (new)" = "Union",
+      "US 7-30's" = "Union",
+      "US 10-40's" = "Union")
 
 get_bankers <- function() {
-  (read.csv(BANKERS_FILE, stringsAsFactors = FALSE) 
-   %>% mutate(src = "Bankers"))   
+  ret <- (read.csv(BANKERS_FILE, stringsAsFactors = FALSE) 
+          %>% mutate(src = "Bankers"))
+  ret[["series"]] <-
+      plyr::revalue(ret[["series"]], RENAME_BANKERS)
+  ret
 }
 
 get_merchants <- function() {
     merchants <- 
       (mutate(read.csv(MERCHANTS_FILE, stringsAsFactors = FALSE), 
              src = "Merchants")
-       %>% filter(! registered)
+       %>% filter(series %in% names(RENAME_MERCHANTS))
        %>% select(- registered)
       )
     merchants[["series"]] <-
-        plyr::revalue(merchants[["series"]],
-                 c("fives_1874"="US_5pct_1874",
-                   "five_twenty_coup"="US_five_twenty",
-                   "oneyr_new"="US_oneyr_new",
-                   "oneyr_old"="US_oneyr_old",
-                   "seven_thirties"="US_seven_thirty",
-                   "sixes_1881_coup"="US_6pct_1881",
-                   "ten_forty"="US_ten_forty"))
+        plyr::revalue(merchants[["series"]], RENAME_MERCHANTS)
     merchants
 }
 
@@ -71,5 +95,7 @@ main <- function() {
     ret$date <- as.Date(ret$date)
     ret$series <- factor(ret$series)
     ret$group <- BOND_GROUPS[as.character(ret$series)]
+    attr(ret, "rename_merchants") <- RENAME_MERCHANTS
+    attr(ret, "rename_bankers") <- RENAME_BANKERS
     ret
 }
